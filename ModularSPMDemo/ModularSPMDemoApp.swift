c3efdec81aaaadc7e6fb60174e68b7267d7ab75b
Papon Supamongkonchai <papon.s@cimbthai.com>
2023-08-03 17:34:56 +0700

    Initial Commit

---
+++ ModularSPMDemo/ModularSPMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularSPMDemoApp.swift
+//  ModularSPMDemo
+//
+//  Created by Papon Supamongkonchai on 3/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ModularSPMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
